1ca3a1816d1bca7a47cc256ba93130ce0d12e4b3
Erhan <demirci.erhan@windowslive.com>
2022-04-16 21:47:27 +0300

    Update server3.js

--- server3.js
+++ server3.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const YouTubeNotifier = require('youtube-notification');
 
+const PORT = process.env.PORT || 1337;
+const HOST = process.env.HOST || '0.0.0.0';
+
 
 const notifier = new YouTubeNotifier({
   hubCallback: 'https://callbackerhan.herokuapp.com/youtube/callback',
@@ -43,8 +46,7 @@ router.get('/myapp/', function(req, res){
 
 
 
-const PORT = process.env.PORT || 1337;
-const HOST = process.env.HOST || '0.0.0.0';
+
 
 app.listen(PORT, HOST, () => {
       console.log('Server started on ' + HOST + ':' + PORT);
