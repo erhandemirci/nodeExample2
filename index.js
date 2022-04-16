dc977ae35c51395cbc9151734a7dfd73d827a4b3
Erhan <demirci.erhan@windowslive.com>
2022-04-16 20:32:40 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var normalizedPath = require("path").join(__dirname, "routes");
+
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  require("./routes/" + file);
+});
+
+// Continue application logic here
