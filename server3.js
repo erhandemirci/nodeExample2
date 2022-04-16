517af84435ee803ab43afa74051e1a31aa742ed2
Erhan <demirci.erhan@windowslive.com>
2022-04-16 21:46:39 +0300

    Update server3.js

--- server3.js
+++ server3.js
@@ -3,13 +3,34 @@ const app = express();
 const YouTubeNotifier = require('youtube-notification');
 
 
+const notifier = new YouTubeNotifier({
+  hubCallback: 'https://callbackerhan.herokuapp.com/youtube/callback',
+  port: PORT,
+ 
+  path: '/youtube'
+});
+
+notifier.on('notified', data => {
+  console.log('New Video');
+  console.log(
+    `${data.channel.name} just uploaded a new video titled: ${data.video.title}`
+  );
+});
+ 
+
+
+
     // get an instance of router
 var router = express.Router();
 
 
     // home page route (http://localhost:8080)
     router.get('/', function(req, res) {
+          notifier.setup();
+    notifier.subscribe('UC1zAttFQKikWoKH3Vb39ETA');
+        
         res.send("google-site-verification=ZPV3ADqzl5x-Z3e_WDGPt1TjgJCzNN3_dxmpDibSy0o");
+        
          
         
     });
