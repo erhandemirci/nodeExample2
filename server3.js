c480d3c498fe09a9c517df2e1898257f33a10393
Erhan <demirci.erhan@windowslive.com>
2022-04-16 20:52:02 +0300

    Update server3.js

--- server3.js
+++ server3.js
@@ -3,8 +3,22 @@ const app = express().use(express.static(__dirname + '/')),
 const http = require('http').Server(app),
 const io = require('socket.io')(http);
 
+
+const YouTubeNotifier = require('youtube-notification');
+
+
+ const PORT = process.env.PORT || 1337;
+const notifier = new YouTubeNotifier({
+  hubCallback: 'https://callbackerhan.herokuapp.com/youtube/callback',
+  port: PORT,
+ 
+  path: '/youtube'
+});
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+    notifier.setup();
+    notifier.subscribe('UC1zAttFQKikWoKH3Vb39ETA');
 });
 
 io.on('connection', function(socket){
@@ -18,17 +32,8 @@ http.listen(port, function(){
 
 
 
-const YouTubeNotifier = require('youtube-notification');
 
 
- const PORT = process.env.PORT || 1337;
-const notifier = new YouTubeNotifier({
-  hubCallback: 'https://callbackerhan.herokuapp.com/youtube/callback',
-  port: PORT,
- 
-  path: '/youtube'
-});
-notifier.setup();
  
 notifier.on('notified', data => {
   console.log('New Video');
@@ -37,4 +42,4 @@ notifier.on('notified', data => {
   );
 });
  
-notifier.subscribe('UC1zAttFQKikWoKH3Vb39ETA');
+
