a2c61570bc2cbbf73d08ed8a725d548664064da2
Erhan <demirci.erhan@windowslive.com>
2022-04-16 20:50:08 +0300

    Update server3.js

--- server3.js
+++ server3.js
@@ -1,3 +1,22 @@
+const express = require('express'),
+app = express().use(express.static(__dirname + '/')),
+http = require('http').Server(app),
+io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(port, function(){
+    console.log("Node server listening on port " + port);
+});
+
+
+
 
 const YouTubeNotifier = require('youtube-notification');
 
