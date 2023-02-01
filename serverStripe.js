9e53d6612d4d7f60988766c9e33ba45c698f91e5
Erhan <demirci.erhan@windowslive.com>
2023-02-01 23:57:39 +0300

    Update serverStripe.js

--- serverStripe.js
+++ serverStripe.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')('sk_test_51MWlHZL5DDIW0DSD2WiCFAKSO5sFRzO62CVFYQF6We9Uk7bz7QrSwh2bgtrnqPN4TH0UQPzPQs4tGqfzPBFqZ8xS00B7PcgqZk');
 // This example sets up an endpoint using the Express framework.
-// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
+// Wrted: https://youtu.be/rPR2aJ6XnAc.
 
 app.post('/payment-sheet', async (req, res) => {
   // Use an existing Customer ID if this is a returning customer.
